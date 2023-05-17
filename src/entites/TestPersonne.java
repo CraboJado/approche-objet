cd6789178d6d91583f0ccc47a7c94607078bba33
liting <liting_qiu@hotmail.com>
2023-05-17 17:24:04 +0200

    TP3 done

--- src/entites/TestPersonne.java
+++ src/entites/TestPersonne.java
@@ -9,7 +9,18 @@ public class TestPersonne {
         Personne per1 = new Personne("kuku","yoyo", ad1);
         Personne per2 = new Personne("bubu","popo", ad2);
 
+        per1.showNameUppercase();
+        String name = per1.getName();
+        System.out.println(name);
+        per1.modifyName("kukuModi");
+        System.out.println(per1.getName());
+
+        AdressePostale ad3 = new AdressePostale(3,"rue brumaire3",35444,"montpellier3");
+        per1.modifyAddress(ad3);
         System.out.println(per1);
+
+        AdressePostale add = per1.getAddress();
+        System.out.println(add);
 
     }
 }
--- src/entites2/Personne.java
+++ src/entites2/Personne.java
@@ -15,6 +15,34 @@ public class Personne {
         ;
     }
 
+    public void showNameUppercase(){
+        System.out.println(name.toUpperCase());
+        System.out.println(firstname.toUpperCase());
+    }
+
+    public void modifyName (String name){
+        this.name = name;
+    }
+
+    public void modifyFirstName (String firstname){
+        this.firstname = firstname;
+    }
+
+    public void modifyAddress (AdressePostale address){
+        this.address = address;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getFirstname(){
+        return firstname;
+    }
+
+    public AdressePostale getAddress(){
+        return address;
+    }
 
     @Override
     public String toString() {
