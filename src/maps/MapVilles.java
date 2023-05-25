4eafc219d88cf4ce35b48a4bb8969108f5c691eb
liting <liting_qiu@hotmail.com>
2023-05-25 15:11:57 +0200

    TP12 done rewrite

--- src/maps/MapVilles.java
+++ src/maps/MapVilles.java
@@ -11,7 +11,7 @@ public class MapVilles {
         HashMap<String, Ville> map = new HashMap<>();
         Ville ville1 = new Ville("Montpellier", 800200);
         Ville ville2 = new Ville("Lyon", 200300);
-        Ville ville3 = new Ville("Lille", 300300);
+        Ville ville3 = new Ville("Lille", 100300);
 
         map.put(ville1.getName(),ville1);
         map.put(ville2.getName(),ville2);
@@ -19,24 +19,31 @@ public class MapVilles {
 
         Iterator<String> keysIte = map.keySet().iterator();
         long min = map.get(ville1.getName()).getNumber();
+        String key =ville1.getName();
 
         while ((keysIte.hasNext())){
             String targetKey = keysIte.next();
             long targetPopulation = map.get(targetKey).getNumber();
             if(targetPopulation < min) {
                 min = targetPopulation;
+                key = targetKey;
             }
         }
 
-        Iterator<String> keysIte2 = map.keySet().iterator();
-        while ((keysIte2.hasNext())){
-            String targetKey = keysIte2.next();
-            long targetPopulation = map.get(targetKey).getNumber();
-            if(targetPopulation == min) {
-                keysIte2.remove();
+//        Iterator<String> keysIte2 = map.keySet().iterator();
+//        while ((keysIte2.hasNext())){
+//            String targetKey = keysIte2.next();
+//            long targetPopulation = map.get(targetKey).getNumber();
+//            if(targetPopulation == min) {
+//                keysIte2.remove();
+//            }
+//        }
+//        String targetKey = "";
+//        for (String ele : map.keySet()) {
+//            if(map.get(ele).getNumber() == min) targetKey = ele;
+//        }
 
-            }
-        }
+        map.remove(key);
         System.out.println(map);
     }
 }
